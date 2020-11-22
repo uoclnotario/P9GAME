5b55bf85eba2ab91b2e46f434cbfb851d7ccf500
uoclnotario <72309408+uoclnotario@users.noreply.github.com>
2020-11-22 17:32:48 +0100

    Proyecto inicial

---
+++ P9Game/Assets/Recursos Globales/Entorno/FixedScroll.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedScroll : MonoBehaviour
+{
+    public float theScrollSpeed = 0.025f;
+
+    public Transform theCamera;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        theCamera.position = new Vector3(theCamera.position.x- theScrollSpeed, theCamera.position.y , theCamera.position.z);
+    }
+}
